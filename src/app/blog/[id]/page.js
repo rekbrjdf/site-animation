965b671e6712fee36fffcf8fbf3c4866895364ca
Kulikova <kulikova.viktori@mail.ru>
2025-02-22 18:05:42 +0400

    commit

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,34 +1,54 @@
-// import React from 'react';
-// import { getApiHost } from 'utils/functions';
-// import BlogHeader from '../components/adaptive/BlogHeader';
-// import BlogContent from '../components/adaptive/BlogContent';
-// import News from '../../components/adaptive/News';
+import React from 'react';
+import { getApiHost } from 'utils/functions';
+import BlogHeader from '../components/adaptive/BlogHeader';
+import BlogContent from '../components/adaptive/BlogContent';
+import News from '../../components/adaptive/News';
 
-// const newsUpdate = [
-//   {
-//     linkedin: 'https://www.linkedin.com/feed/update/urn:li:activity:7160572889300160512/',
-//   },
-// ];
-// //
-// async function BlogDetailed({ params }) {
-//   const { id } = params;
+const newsUpdate = [
+  {
+    linkedin: 'https://www.linkedin.com/feed/update/urn:li:activity:7160572889300160512/',
+  },
+];
 
-//   const news = await (await fetch(`${getApiHost()}/news-posts/${id}`)).json();
 
-//   return (
-//     <main>
-//       <BlogHeader
-//         avatar={news.author.avatar}
-//         position={news.author.position}
-//         author={news.author.name}
-//         title={news.title}
-//         date={news.publish}
-//         tag={news.news_category.title}
-//       />
-//       <BlogContent content={news.article} linkedin={newsUpdate[0].linkedin} />
-//       <News titleMain="Other news" />
-//     </main>
-//   );
-// }
+export async function generateStaticParams() {
+  // Получите список ID постов блога из вашего API или источника данных
+  const response = await fetch(`${getApiHost()}/news-posts`);
+  const posts = await response.json();
 
-// export default BlogDetailed;
+  // Верните массив объектов параметров
+  return posts.map((post) => ({
+    id: post.id.toString(),
+  }));
+}
+
+export default async function BlogPost({ params }) {
+  const { id } = params;
+  const post = await fetch(`${getApiHost()}/news-posts/${id}`).then((res) => res.json());
+
+  // Отобразите содержимое вашего поста в блоге
+  return <div>{post.title}</div>;
+}
+
+async function BlogDetailed({ params }) {
+  const { id } = params;
+
+  const news = await (await fetch(`${getApiHost()}/news-posts/${id}`)).json();
+
+  return (
+    <main>
+      <BlogHeader
+        avatar={news.author.avatar}
+        position={news.author.position}
+        author={news.author.name}
+        title={news.title}
+        date={news.publish}
+        tag={news.news_category.title}
+      />
+      <BlogContent content={news.article} linkedin={newsUpdate[0].linkedin} />
+      <News titleMain="Other news" />
+    </main>
+  );
+}
+
+export default BlogDetailed;
